ff5ba774ccb9814a87a889ecc45953403d24c279
Daniel Santos <80872981+ddanielsantos@users.noreply.github.com>
2023-07-08 20:16:49 -0300

    chore: format

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,7 +1,5 @@
-import React from "react"
+import React from "react";
 
 export const Notes = () => {
-    return (
-        <p>todo implement</p>
-    )
-}
+	return <p>todo implement</p>;
+};
--- src/scripts/mdx-to-presentation.ts
+++ src/scripts/mdx-to-presentation.ts
@@ -1,55 +1,54 @@
-import fs from 'node:fs'
-import path from 'node:path'
-
+import fs from "node:fs";
+import path from "node:path";
 
 export function mdxToSlides(): string[] {
-    const p = path.resolve('deck.mdx')
-    const content = fs.readFileSync(p, 'utf-8')
-    return content.split('---')
+	const p = path.resolve("deck.mdx");
+	const content = fs.readFileSync(p, "utf-8");
+	return content.split("---");
 }
 
 function getComponentsFromSlide(slide: string): string[] {
-    const components = slide.match(/<([A-Z][a-z]+)/g)
-    if (!components) {
-        return []
-    }
+	const components = slide.match(/<([A-Z][a-z]+)/g);
+	if (!components) {
+		return [];
+	}
 
-    return components.map(component => {
-        return component.replace('<', '').replace(' ', '')
-    })
+	return components.map((component) => {
+		return component.replace("<", "").replace(" ", "");
+	});
 }
 
 function getImportStrings(slide: string): string {
-    const components = getComponentsFromSlide(slide)
-    
-    const imports = components.map(component => {
-        return `import { ${component} } from '../../components/${component}'`
-    })
+	const components = getComponentsFromSlide(slide);
 
-    return '---\n' + imports.join('\n') + '\n---'
+	const imports = components.map((component) => {
+		return `import { ${component} } from '../../components/${component}'`;
+	});
+
+	return "---\n" + imports.join("\n") + "\n---";
 }
 
 export function mdxToPresentation() {
-    createSlidesFolder()
+	createSlidesFolder();
 
-    mdxToSlides().forEach((slide, index) => {
-        const file_path = path.resolve(`src/pages/slides/${index}.astro`)
+	mdxToSlides().forEach((slide, index) => {
+		const file_path = path.resolve(`src/pages/slides/${index}.astro`);
 
-        const content = getImportStrings(slide) + '\n\n' + slide
+		const content = getImportStrings(slide) + "\n\n" + slide;
 
-        fs.writeFileSync(file_path, content, {
-            flag: 'w+'
-        })
-    })
-    
-    console.log('Done!')
+		fs.writeFileSync(file_path, content, {
+			flag: "w+",
+		});
+	});
+
+	console.log("Done!");
 }
 
 function createSlidesFolder() {
-    const slides_folder = path.resolve('src/pages/slides')
-    if (!fs.existsSync(slides_folder)) {
-        fs.mkdirSync(slides_folder)
-    }
+	const slides_folder = path.resolve("src/pages/slides");
+	if (!fs.existsSync(slides_folder)) {
+		fs.mkdirSync(slides_folder);
+	}
 }
 
-mdxToPresentation()
+mdxToPresentation();

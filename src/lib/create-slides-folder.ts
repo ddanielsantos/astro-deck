33e54daa2f48f138389fa91ad9b5d3f5b60f3040
Daniel Santos <80872981+ddanielsantos@users.noreply.github.com>
2023-07-15 00:31:49 -0300

    feat: hot reload (#28)
    * chore: add watch-and-run Vite plugin
    
    * refactor: decouple
    
    * feat: nuke folder if exists

--- src/lib/create-slides-folder.ts
+++ src/lib/create-slides-folder.ts
@@ -5,5 +5,11 @@ export function createSlidesFolder() {
 	const slides_folder = path.resolve("src/pages/slides");
 	if (!fs.existsSync(slides_folder)) {
 		fs.mkdirSync(slides_folder);
+		return;
+	}
+
+	const files = fs.readdirSync(slides_folder);
+	for (const file of files) {
+		fs.rmSync(path.resolve(slides_folder, file));
 	}
 }

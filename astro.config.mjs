bc25612893eb5736a1a5f203bc7b195d5af6d6e7
Daniel Santos <80872981+ddanielsantos@users.noreply.github.com>
2023-07-23 01:19:30 -0300

    feat: transition api (#32)
    * chore: update astro
    
    * feat: use view transitions from astro
    
    * chore: provide a simpler deck.mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,5 +20,8 @@ export default defineConfig({
                 }
             ])
         ]
+    },
+    experimental: {
+        viewTransitions: true
     }
 });
---
+++ src/components/SlideControls.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+interface ButtonProps {
+    direction: 'next' | 'prev'
+    shouldShow: boolean
+    href: string
+    children?: React.ReactNode
+    debug?: boolean
+}
+
+const Button = (props: ButtonProps) => {
+    if (!props.shouldShow) {
+        return null
+    }
+
+    const directionBasedProps = props.direction === 'prev' ? {left: '0'} : {right: '0'}
+    const debugProps = props.debug ? {outline: "1px solid red"} : {}
+
+    return (
+        <a href={props.href} style={{
+            position: 'absolute',
+            top: '0',
+            width: '200px',
+            height: '100%',
+            // TODO: handle this later (custom children idk)
+            opacity: 0,
+            ...directionBasedProps,
+            ...debugProps,
+        }}>{props.children || props.direction}</a>
+    )
+}
+
+interface Props {
+    hasPrev: boolean;
+    hasNext: boolean;
+    prevSlideUrl: string
+    nextSlideUrl: string
+}
+
+export const SlideControls = ({hasNext, hasPrev, nextSlideUrl, prevSlideUrl}: Props) => {
+    return (
+        <>
+            {/* {hasPrev && <a href={prevSlideUrl}>prev</a>} */}
+            <Button direction="prev" href={prevSlideUrl} shouldShow={hasPrev}/>
+            <Button direction="next" href={nextSlideUrl} shouldShow={hasNext}/>
+        </>
+    )
+}
